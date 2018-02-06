d6ac90f47c8f7e136974c0de930b82f35e71428e
chuyouhong <chuyh@medishare.cn>
2018-02-06 15:40:38 +0800

    update

--- src/main/java/first/controller/image/OcrTest.java
+++ src/main/java/first/controller/image/OcrTest.java
@@ -1,22 +1,22 @@
-package first.controller.image;
-
-import java.io.File;
-import java.io.IOException;
-
-public class OcrTest {
-
-	public static void main(String[] args) {
-		String path = "D:/test/医生头像/1.png";
-		System.out.println("ORC Test Begin......");
-		try {
-			String valCode = new OCR().recognizeText(new File(path), "png");
-			System.out.println(valCode);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("ORC Test End......");
-	}
-
-}
+//package first.controller.image;
+//
+//import java.io.File;
+//import java.io.IOException;
+//
+//public class OcrTest {
+//
+//	public static void main(String[] args) {
+//		String path = "D:/test/医生头像/1.png";
+//		System.out.println("ORC Test Begin......");
+//		try {
+//			String valCode = new OCR().recognizeText(new File(path), "png");
+//			System.out.println(valCode);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("ORC Test End......");
+//	}
+//
+//}
--- src/main/java/first/controller/image/Test.java
+++ src/main/java/first/controller/image/Test.java
@@ -1,27 +1,27 @@
-package first.controller.image;
-
-import java.io.File;
-
-public class Test {
-	public static void main(String[] args) {
-		try {
-
-			File testDataDir = new File("D:/test/医生头像");
-			System.out.println(testDataDir.listFiles().length);
-			int i = 0;
-			for (File file : testDataDir.listFiles()) {
-				i++;
-				String recognizeText = new OCRHelper().recognizeText(file);
-				System.out.print(recognizeText + "\t");
-
-				if (i % 5 == 0) {
-					System.out.println();
-				}
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-}
+//package first.controller.image;
+//
+//import java.io.File;
+//
+//public class Test {
+//	public static void main(String[] args) {
+//		try {
+//
+//			File testDataDir = new File("D:/test/医生头像");
+//			System.out.println(testDataDir.listFiles().length);
+//			int i = 0;
+//			for (File file : testDataDir.listFiles()) {
+//				i++;
+//				String recognizeText = new OCRHelper().recognizeText(file);
+//				System.out.print(recognizeText + "\t");
+//
+//				if (i % 5 == 0) {
+//					System.out.println();
+//				}
+//			}
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+//}
